56faecd75355dd34e13217a5afe338292eca199c
Zan Zibert <zan_zibert@hotmail.com>
2020-08-05 14:19:11 +0200

    adding the text interface

--- data/data.go
+++ data/data.go
@@ -2,16 +2,29 @@ package data
 
 import "database/sql"
 
+type Text interface {
+	fetch(id int) (err error)
+	create() (err error)
+	update() (err error)
+	delete() (err error)
+}
+
 type User struct {
 	Db       *sql.DB
 	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
+	Group    *Group `json:"group"`
 }
 
 type Group struct {
-	Db   *sql.DB
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Db    *sql.DB
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Users []User `json:"users"`
 }
+
+// GROUP FUNCTIONS
+
+// USER FUNCTIONS
