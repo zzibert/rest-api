6985239feec0dade6b6e5b67b65b270247a45b8e
Zan Zibert <zan_zibert@hotmail.com>
2020-08-05 14:48:26 +0200

    cascade table groups if exists

--- docker/init.sql
+++ docker/init.sql
@@ -1,11 +1,14 @@
-CREATE TABLE [IF NOT EXISTS] groups (
+drop table groups cascade if exists;
+drop table users if exists;
+
+CREATE TABLE groups (
   id serial PRIMARY KEY,
   name varchar(50) UNIQUE NOT NULL,
 );
 
-CREATE TABLE [IF NOT EXISTS] users (
+CREATE TABLE users (
   id serial PRIMARY KEY,
-  name varchar(50) UNIQUE NOT NULL,
+  name varchar(50) NOT NULL,
   password varchar(50) NOT NULL,
   email varchar(255) UNIQUE NOT NULL,
   group_id integer references groups(id)
