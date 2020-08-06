22f67290110624a186e9c860d5a589b4988cd2fa
Zan Zibert <zan_zibert@hotmail.com>
2020-08-06 12:10:43 +0200

    making handler functions more generic

--- server.go
+++ server.go
@@ -40,98 +40,19 @@ func handleGroupRequest(t Text) http.HandlerFunc {
 		var err error
 		switch r.Method {
 		case "GET":
-			err = handleGroupGet(w, r, t)
+			err = handleGet(w, r, t)
 		case "POST":
-			err = handleGroupPost(w, r, t)
+			err = handlePost(w, r, t)
 		case "PUT":
-			err = handleGroupPut(w, r, t)
+			err = handlePut(w, r, t)
 		case "DELETE":
-			err = handleGroupDelete(w, r, t)
+			err = handleDelete(w, r, t)
 		}
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
-}
-
-func handleGroupGet(w http.ResponseWriter, r *http.Request, group Text) (err error) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return
-	}
-
-	err = group.fetch(id)
-	if err != nil {
-		return
-	}
-
-	output, err := json.MarshalIndent(&group, "", "\t\t")
-	if err != nil {
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(output)
-	return
-}
-
-func handleGroupPost(w http.ResponseWriter, r *http.Request, group Text) (err error) {
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-
-	json.Unmarshal(body, &group)
-	err = group.create()
-	if err != nil {
-		return
-	}
-	w.WriteHeader(200)
-	return
-}
-
-func handleGroupPut(w http.ResponseWriter, r *http.Request, group Text) (err error) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return
-	}
-
-	err = group.fetch()
-	if err != nil {
-		return
-	}
-
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-	json.Unmarshal(body, &group)
-	err = group.update()
-	if err != nil {
-		return
-	}
-
-	w.WriteHeader(200)
-	return
-}
-
-func handleGroupDelete(w http.ResponseWriter, r *http.Request, group Text) (err error) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return
-	}
-
-	err = group.fetch(id)
-	if err != nil {
-		return
-	}
-
-	err = group.delete()
-	if err != nil {
-		return
-	}
-
-	w.WriteHeader(200)
-	return
 }
 
 // USER HANDLER FUNCTIONS
@@ -141,17 +62,98 @@ func handleUserRequest(t Text) http.HandlerFunc {
 		var err error
 		switch r.Method {
 		case "GET":
-			err = handleUserGet(w, r, t)
+			err = handleGet(w, r, t)
 		case "POST":
-			err = handleUserPost(w, r, t)
+			err = handlePost(w, r, t)
 		case "PUT":
-			err = handleUserPut(w, r, t)
+			err = handlePut(w, r, t)
 		case "DELETE":
-			err = handleUserDelete(w, r, t)
+			err = handleDelete(w, r, t)
 		}
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
+}
+
+//GENERIC HANDLER FUNCTIONS
+
+func handleGet(w http.ResponseWriter, r *http.Request, text Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	err = text.fetch(id)
+	if err != nil {
+		return
+	}
+
+	output, err := json.MarshalIndent(&text, "", "\t\t")
+	if err != nil {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request, text Text) (err error) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &text)
+	err = text.create()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request, text Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	err = text.fetch(id)
+	if err != nil {
+		return
+	}
+
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	json.Unmarshal(body, &text)
+	err = text.update()
+	if err != nil {
+		return
+	}
+
+	w.WriteHeader(200)
+	return
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request, text Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	err = text.fetch(id)
+	if err != nil {
+		return
+	}
+
+	err = text.delete()
+	if err != nil {
+		return
+	}
+
+	w.WriteHeader(200)
+	return
 }
