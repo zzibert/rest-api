199bb625965d7db3b50b297867c72655ba52e9d8
Zan Zibert <zan_zibert@hotmail.com>
2020-08-09 16:16:51 +0200

    created sample test for handleGroupGet

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	. "github.com/zzibert/rest-api/data"
@@ -10,4 +12,17 @@ import (
 func TestHandleGroupGet(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/group/", handleGroupRequest(&TestGroup{}))
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/group/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+	var group Group
+	json.Unmarshal(writer.Body.Bytes(), &group)
+	if group.Id != 1 {
+		t.Errorf("Cannot retrieve JSON group")
+	}
 }
